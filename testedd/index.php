656739bf7671c91b3d95213facf7c63dfc51fa4a
Felipe Lima Flores <limaflores@gmail.com>
2019-07-24 00:39:37 -0300

    terceiro

---
+++ testedd/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="content-language" content="pt-br" />
+		<title>Crud</title>
+		<link href="estilos/css.css" rel="stylesheet" type="text/css" media="all"/>
+		<!-- @Felipe Lima Flores 2019-->
+	</head>
+	<body>
+		<div class="barra">
+			<h3>Crud</h3>
+			<h4>Selecione uma opção por favor</h4>
+		</div>
+		<div  class="selecao">
+			<a href="insere.php" class="links">Inserir</a>
+			<a href="uptade.php" class="links">Editar</a>
+			<a href="delete.php" class="links">Deletar</a>
+			<a href="select.php" class="links">Pesquisar</a>
+		</div>
+	</body>
+</html>
+
+		
+
+
+
+
--- testedd/insere.php
+++ testedd/insere.php
@@ -3,41 +3,15 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta http-equiv="content-language" content="pt-br" />
-		<title>Treino - Layout Responsivo</title>
-		<link href="css/geral.css" rel="stylesheet" type="text/css" media="all"/>
-
+		<title>Crud</title>
+		<link href="estilos/css.css" rel="stylesheet" type="text/css" media="all"/>
+		<!-- @Felipe Lima Flores 2019-->
 	</head>
-		<body>
-
-		
+	<body>
 		<?php
-		/*
-		//include "caminho" mas deixa passar
-
-		
-		//require diz que precisa, é obrigatório
-		require ("conecta.php");
-
-
-		$nome = "Felipe";
-		$data_nascimento = "1990-06-19";
-
-		//mysqli_query($link, "insert into pessoas set nome='$nome', data_nascimento='$data_nascimento'");
-
-
-		for ($i=1; $i <= 8 ; $i++) { 
-			$novonome = $nome.$i;
-			mysqli_query($link, "insert into pessoas set nome='$novonome', data_nascimento='$data_nascimento'");
-		}
-
-
-		//Acrescentar o campo número de semanas.
-		*/
 		$valorVindoBD="teste";
 		?>
 	
-
-
 		<form action="recebeinsere.php" method"="post>
 			<?php for ($i=1; $i <= 8 ; $i++) { ?>
 				<select name="teste[]">
@@ -47,9 +21,9 @@
 			<input type="submit" value"ok" name="ok" />
 		</form>
 
-
 	</body>
 </html>
+
 
 		
 
